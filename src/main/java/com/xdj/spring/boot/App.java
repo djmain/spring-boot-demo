97856b0021b730f04058fc0aa8b4dcc2296431f3
djmain <djmain@126.com>
2020-03-22 23:01:06 +0800

    测试邮件服务

--- src/main/java/com/xdj/spring/boot/App.java
+++ src/main/java/com/xdj/spring/boot/App.java
@@ -2,15 +2,36 @@ package com.xdj.spring.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.StopWatch;
 
 
-@SpringBootApplication
-//@RestController
+@SpringBootApplication(scanBasePackages = "com.xdj.spring.boot")
 public class App {
 
-
     public static void main(String[] args){
+        try
+        {
+            test();
+        }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
         SpringApplication.run(App.class, args);
     }
 
+    public static void test() throws InterruptedException
+    {
+        StopWatch sw = new StopWatch("test");
+        sw.start("task1");
+        // do something
+        Thread.sleep(100);
+        sw.stop();
+        sw.start("task2");
+        // do something
+        Thread.sleep(200);
+        sw.stop();
+        System.out.println("sw.prettyPrint()~~~~~~~~~~~~~~~~~");
+        System.out.println(sw.prettyPrint());
+    }
 }
